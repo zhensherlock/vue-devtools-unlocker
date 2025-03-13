4e1e581d92722fd624c9458bf191a9f2dc2adb81
zhensherlock <zhensherlock@126.com>
2025-03-13 13:33:49 +0800

    ci: update pack command

--- packages/chrome-extension/config/pack.js
+++ packages/chrome-extension/config/pack.js
@@ -1,15 +1,14 @@
 const { readFileSync, existsSync, mkdirSync } = require('fs');
-const { parse, resolve } = require('path');
+const { resolve } = require('path');
 const AdmZip = require('adm-zip');
 
 try {
-  const { base } = parse(__dirname);
-  const { version } = JSON.parse(
-    readFileSync(resolve(__dirname, 'build', 'manifest.json'), 'utf8')
+  const { version, name } = JSON.parse(
+    readFileSync(resolve(__dirname, '../build', 'manifest.json'), 'utf8')
   );
 
   const outdir = 'release';
-  const filename = `${base}-v${version}.zip`;
+  const filename = `${name}-v${version}.zip`;
   const zip = new AdmZip();
   zip.addLocalFolder('build');
   if (!existsSync(outdir)) {
@@ -21,5 +20,5 @@ try {
     `Success! Created a ${filename} file under ${outdir} directory. You can upload this file to web store.`
   );
 } catch (e) {
-  console.error('Error! Failed to generate a zip file.');
+  console.error('Error! Failed to generate a zip file.', e);
 }
