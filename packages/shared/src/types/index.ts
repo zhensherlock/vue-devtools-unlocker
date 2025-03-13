c2b86dec964e518a5d1e8a03325cefd880e107e6
zhensherlock <zhensherlock@126.com>
2025-03-13 14:12:11 +0800

    feat: add shared types

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,14 +1,9 @@
-interface Window {
-  __VUE_DEVTOOLS_GLOBAL_HOOK__: VueDevtoolsHook;
-  __VUE__: boolean;
-}
-
-interface VueDevtoolsHook {
+export interface VueDevtoolsHook {
   enabled?: boolean;
   emit: (event: string, ...args: unknown[]) => void;
 }
 
-interface Vue2Instance {
+export interface Vue2Instance {
   super?: Vue2Instance;
   version: string;
   config: {
@@ -24,8 +19,8 @@ interface Vue2Instance {
   };
 }
 
-interface Vue3Instance {
+export interface Vue3Instance {
   version: string;
 }
 
-interface VueInstance extends Vue2Instance, Vue3Instance {}
+export interface VueInstance extends Vue2Instance, Vue3Instance {}
