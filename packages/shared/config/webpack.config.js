39af968e7a005190c3fc042484f5d663b428b1e5
zhensherlock <zhensherlock@126.com>
2025-03-13 11:48:02 +0800

    ci: add terser plugin

--- packages/shared/config/webpack.config.js
+++ packages/shared/config/webpack.config.js
@@ -1,17 +1,30 @@
 'use strict';
 
+const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 const PATHS = require('./paths');
 
 // Merge webpack configuration files
-const config = (env, argv) =>
-  merge(common, {
+const config = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return merge(common, {
     entry: {
       main: PATHS.src + '/main.ts',
     },
-    devtool: argv.mode === 'production' ? false : 'source-map',
+    devtool: isProduction ? false : 'source-map',
+    optimization: {
+      minimize: isProduction,
+      minimizer: [new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      })],
+    },
   });
+};
 
 module.exports = config;
